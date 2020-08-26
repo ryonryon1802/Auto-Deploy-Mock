92e1d79888cd351a45cc72bd1f41ac0961adaf0d
ryonryon1802 <ryo.hskw0812@gmail.com>
2020-08-26 15:03:51 +0000

    add: Add regist function to save virtual machine data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/index', to: 'virtual_machine#index'
+  get '/new', to: 'virtual_machine#new'
+  post '/create', to: 'virtual_machine#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
